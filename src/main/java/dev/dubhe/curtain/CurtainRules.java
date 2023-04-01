9bb47e289e60d8c0fc07fb9013cead6a088030c8
DancingSnow <1121149616@qq.com>
2023-04-01 20:56:44 +0800

    add viewDistance rule

--- src/main/java/dev/dubhe/curtain/CurtainRules.java
+++ src/main/java/dev/dubhe/curtain/CurtainRules.java
@@ -5,8 +5,13 @@ import dev.dubhe.curtain.api.rules.Rule;
 import dev.dubhe.curtain.api.rules.Validator;
 import dev.dubhe.curtain.utils.TranslationHelper;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.ServerInterface;
 
-import static dev.dubhe.curtain.api.rules.Categories.*;
+import static dev.dubhe.curtain.api.rules.Categories.CREATIVE;
+import static dev.dubhe.curtain.api.rules.Categories.FEATURE;
+
 
 public class CurtainRules {
     public static class LanguageValidator implements Validator<String> {
@@ -22,4 +27,40 @@ public class CurtainRules {
             suggestions = {"zh_cn", "en_us"}
     )
     public static String language = "zh_cn";
+
+    public static class ViewDistanceValidator implements Validator<Integer> {
+
+        @Override
+        public boolean validate(CommandSourceStack source, CurtainRule<Integer> rule, String newValue) {
+            int value;
+            try {
+                value = Integer.parseInt(newValue);
+            } catch (NumberFormatException e) {
+                return false;
+            }
+            if (value < 0 || value > 32) {
+                source.sendFailure(Component.literal("view distance has to be between 0 and 32"));
+                return false;
+            }
+            MinecraftServer server = source.getServer();
+            if (server.isDedicatedServer()) {
+                int vd = (value > 2) ? value : ((ServerInterface) server).getProperties().viewDistance;
+                if (vd != server.getPlayerList().getViewDistance()) {
+                    server.getPlayerList().setViewDistance(vd);
+                }
+
+                return true;
+            } else {
+                source.sendFailure(Component.literal("view distance can only be changed on a server"));
+                return false;
+            }
+        }
+    }
+
+    @Rule(
+            categories = CREATIVE,
+            validators = ViewDistanceValidator.class,
+            suggestions = {"0", "12", "16", "32"}
+    )
+    public static Integer viewDistance = 0;
 }
