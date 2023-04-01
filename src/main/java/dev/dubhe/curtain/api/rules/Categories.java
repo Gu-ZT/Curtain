cb16e6547bd65e8f3cb36b1ffbbf786da9ca83a7
DancingSnow <1121149616@qq.com>
2023-04-02 00:02:14 +0800

    完成fillUpdates相关的mixin

--- src/main/java/dev/dubhe/curtain/api/rules/Categories.java
+++ src/main/java/dev/dubhe/curtain/api/rules/Categories.java
@@ -4,4 +4,5 @@ public class Categories {
     public static final String FEATURE = "feature";
     public static final String CREATIVE = "creative";
     public static final String COMMAND = "command";
+    public static final String SURVIVAL = "survival";
 }
---
+++ src/main/java/dev/dubhe/curtain/mixins/Rule_fillUpdates/FillCommandMixin.java
@@ -0,0 +1,26 @@
+package dev.dubhe.curtain.mixins.Rule_fillUpdates;
+
+import dev.dubhe.curtain.CurtainRules;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.commands.FillCommand;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.Block;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(FillCommand.class)
+public class FillCommandMixin {
+    @Redirect(
+            method = "fillBlocks",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/level/ServerLevel;blockUpdated(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/Block;)V"
+            )
+    )
+    private static void conditionalUpdating(ServerLevel serverWorld, BlockPos blockPos, Block block) {
+        if (CurtainRules.fillUpdates) {
+            serverWorld.blockUpdated(blockPos, block);
+        }
+    }
+}
