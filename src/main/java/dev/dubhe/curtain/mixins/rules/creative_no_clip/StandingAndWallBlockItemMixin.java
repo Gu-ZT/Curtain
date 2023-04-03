506509b188b5d7f88da3441d1a16c3d4d5b65c7b
DancingSnow <1121149616@qq.com>
2023-04-03 18:50:02 +0800

    feat(creative_no_clip): 实现规则creativeNoClip

---
+++ src/main/java/dev/dubhe/curtain/mixins/rules/creative_no_clip/StandingAndWallBlockItemMixin.java
@@ -0,0 +1,33 @@
+package dev.dubhe.curtain.mixins.rules.creative_no_clip;
+
+import dev.dubhe.curtain.CurtainRules;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.StandingAndWallBlockItem;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(StandingAndWallBlockItem.class)
+public class StandingAndWallBlockItemMixin {
+    @Redirect(
+            method = "getPlacementState",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/LevelReader;isUnobstructed(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Z"
+            )
+    )
+    private boolean canCreativePlayerPlace(LevelReader worldView, BlockState state, BlockPos pos, CollisionContext context, BlockPlaceContext itemContext) {
+        Player player = itemContext.getPlayer();
+        if (CurtainRules.creativeNoClip && player != null && player.isCreative() && player.getAbilities().flying) {
+            VoxelShape voxelShape = state.getCollisionShape(worldView, pos, context);
+            return voxelShape.isEmpty() || worldView.isUnobstructed(player, voxelShape.move(pos.getX(), pos.getY(), pos.getZ()));
+        }
+        return worldView.isUnobstructed(state, pos, context);
+    }
+}
---
+++ src/main/java/dev/dubhe/curtain/mixins/rules/creative_no_clip/TheEndGatewayBlockEntityMixin.java
@@ -0,0 +1,19 @@
+package dev.dubhe.curtain.mixins.rules.creative_no_clip;
+
+import dev.dubhe.curtain.CurtainRules;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.block.entity.TheEndGatewayBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(TheEndGatewayBlockEntity.class)
+public class TheEndGatewayBlockEntityMixin {
+    @Inject(method = "canEntityTeleport", cancellable = true, at = @At("HEAD"))
+    private static void checkFlyingCreative(Entity entity, CallbackInfoReturnable<Boolean> cir) {
+        if (CurtainRules.isCreativeFlying(entity)) {
+            cir.setReturnValue(false);
+        }
+    }
+}
