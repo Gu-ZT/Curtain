ba379650942d10b8d23b0ae298d4b456afcc4503
Gu_ZT <administration@ztxy666.cn>
2023-04-04 23:21:37 +0800

    feat(curtainRulesGenerator): 修改规则文件的生成器

--- src/main/java/dev/dubhe/curtain/utils/CurtainRulesGenerator.java
+++ src/main/java/dev/dubhe/curtain/utils/CurtainRulesGenerator.java
@@ -1,21 +1,18 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
+package dev.dubhe.curtain.utils;
 
-package dev.dubhe.curtain;
-
+import dev.dubhe.curtain.CurtainRules;
 import dev.dubhe.curtain.api.rules.CurtainRule;
 import dev.dubhe.curtain.api.rules.RuleManager;
 import dev.dubhe.curtain.utils.TranslationHelper;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Iterator;
 import java.util.Map;
 
-public class CurtainRulesGenerator implements ICurtain {
+public class CurtainRulesGenerator {
     public CurtainRulesGenerator() {
     }
 
@@ -23,38 +20,26 @@ public class CurtainRulesGenerator implements ICurtain {
         RuleManager.addRules(CurtainRules.class);
         setTrans();
         StringBuilder sb = new StringBuilder("* Add Features\n");
-        Iterator var2 = RuleManager.RULES.entrySet().iterator();
 
-        while (var2.hasNext()) {
-            Map.Entry<String, CurtainRule<?>> entry = (Map.Entry) var2.next();
+        for (Map.Entry<String, CurtainRule<?>> entry : RuleManager.RULES.entrySet()) {
             CurtainRule<?> rule = entry.getValue();
-            sb.append("    * ").append((String) entry.getKey()).append("\n");
-            sb.append("        * 名称：`").append(TranslationHelper.translate(rule.getNameTranslationKey(), new Object[0]).getString()).append("`\n");
-            sb.append("        * 描述：`").append(TranslationHelper.translate(rule.getDescTranslationKey(), new Object[0]).getString()).append("`\n");
+            sb.append("    * ").append(entry.getKey()).append("\n");
+            sb.append("        * 名称：`").append(TranslationHelper.translate(rule.getNameTranslationKey()).getString()).append("`\n");
+            sb.append("        * 描述：`").append(TranslationHelper.translate(rule.getDescTranslationKey()).getString()).append("`\n");
             sb.append("        * 类型：`").append(getTypeString(rule.getType())).append("`\n");
             sb.append("        * 默认：`").append(rule.getDefaultValue()).append("`\n");
             String[] suggestions = rule.getSuggestions();
             StringBuilder suggestion = new StringBuilder();
-            String[] var7 = suggestions;
-            int var8 = suggestions.length;
-
-            for (int var9 = 0; var9 < var8; ++var9) {
-                String s = var7[var9];
+            for (String s : suggestions) {
                 suggestion.append("`").append(s).append("`").append(", ");
             }
-
             String str = suggestion.toString();
             sb.append("        * 建议：").append(str, 0, str.length() - 2).append("\n");
             String[] categories = rule.getCategories();
             StringBuilder category = new StringBuilder();
-            String[] var21 = categories;
-            int var11 = categories.length;
-
-            for (int var12 = 0; var12 < var11; ++var12) {
-                String s = var21[var12];
-                category.append("`").append(TranslationHelper.translate("%s.categories.%s".formatted("curtain", s), new Object[0]).getString()).append("`").append(", ");
+            for (String s : categories) {
+                category.append("`").append(TranslationHelper.translate("%s.categories.%s".formatted("curtain", s)).getString()).append("`").append(", ");
             }
-
             str = category.toString();
             sb.append("        * 分类：").append(str, 0, str.length() - 2).append("\n");
         }
@@ -90,7 +75,8 @@ public class CurtainRulesGenerator implements ICurtain {
         TranslationHelper.addTransMap("en_us", TranslationHelper.getTranslationFromResourcePath(stream));
     }
 
-    private static String getTypeString(Class<?> clazz) {
+    @Contract(pure = true)
+    private static @Nullable String getTypeString(Class<?> clazz) {
         if (clazz != Boolean.TYPE && clazz != Boolean.class) {
             if (clazz != Integer.TYPE && clazz != Integer.class) {
                 if (clazz == String.class) {
