150d3241d48114cddd91daacb6676dcfdb1a46f4
Gu_ZT <administration@ztxy666.cn>
2023-07-15 02:25:29 +0800

    feat(betterWoodStrip): 添加`更好的原木去皮`功能
    仅允许名称中包含“去皮”的斧头对原木去皮

--- src/main/java/dev/dubhe/curtain/CurtainRules.java
+++ src/main/java/dev/dubhe/curtain/CurtainRules.java
@@ -15,6 +15,7 @@ import net.minecraft.world.entity.player.Player;
 import static dev.dubhe.curtain.api.rules.Categories.*;
 
 
+@SuppressWarnings("unused")
 public class CurtainRules {
     public static final ThreadLocal<Boolean> impendingFillSkipUpdates = ThreadLocal.withInitial(() -> false);
 
@@ -334,4 +335,10 @@ public class CurtainRules {
             suggestions = {"true", "false"}
     )
     public static boolean betterSignInteraction = false;
+
+    @Rule(
+            categories = {SURVIVAL},
+            suggestions = {"true", "false"}
+    )
+    public static boolean betterWoodStrip = false;
 }
---
+++ src/main/java/dev/dubhe/curtain/mixins/rules/better_wood_strip/AxeItemMixin.java
@@ -0,0 +1,23 @@
+package dev.dubhe.curtain.mixins.rules.better_wood_strip;
+
+import dev.dubhe.curtain.CurtainRules;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.context.UseOnContext;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AxeItem.class)
+public abstract class AxeItemMixin {
+    @Inject(method = "useOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;getToolModifiedState(Lnet/minecraft/world/item/context/UseOnContext;Lnet/minecraftforge/common/ToolAction;Z)Lnet/minecraft/world/level/block/state/BlockState;", ordinal = 0), cancellable = true)
+    private void stripped(UseOnContext context, CallbackInfoReturnable<InteractionResult> cir) {
+        ItemStack itemStack = context.getItemInHand();
+        String name = itemStack.getHoverName().getString();
+        if (!name.contains("Strip") && !name.contains("去皮") && CurtainRules.betterWoodStrip) {
+            cir.setReturnValue(InteractionResult.PASS);
+        }
+    }
+}
