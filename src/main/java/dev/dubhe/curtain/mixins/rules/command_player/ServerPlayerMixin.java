c1bd501492b88b999be2d10990dc91c935ab9a29
Gu_ZT <administration@ztxy666.cn>
2023-08-09 18:27:19 +0800

    fix(resolved): be resolved

--- src/main/java/dev/dubhe/curtain/mixins/rules/command_player/ServerPlayerMixin.java
+++ src/main/java/dev/dubhe/curtain/mixins/rules/command_player/ServerPlayerMixin.java
@@ -6,11 +6,8 @@ import dev.dubhe.curtain.features.player.helpers.EntityPlayerActionPack;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-<<<<<<< HEAD:src/main/java/dev/dubhe/curtain/mixins/ServerPlayerMixin.java
-=======
 import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
->>>>>>> a627c718d1289379cb1bdfcbd950f41dca3e016b:src/main/java/dev/dubhe/curtain/mixins/rules/command_player/ServerPlayerMixin.java
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -30,15 +27,11 @@ public class ServerPlayerMixin implements IServerPlayer {
     }
 
     @Inject(method = "<init>", at = @At(value = "RETURN"))
-<<<<<<< HEAD:src/main/java/dev/dubhe/curtain/mixins/ServerPlayerMixin.java
-    private void onServerPlayerEntityContructor(MinecraftServer p_254143_, ServerLevel p_254435_, GameProfile p_253651_, CallbackInfo ci) {
-=======
     private void onServerPlayerEntityContructor(
             MinecraftServer minecraftServer_1,
             ServerLevel serverWorld_1,
             GameProfile gameProfile_1,
             CallbackInfo ci) {
->>>>>>> a627c718d1289379cb1bdfcbd950f41dca3e016b:src/main/java/dev/dubhe/curtain/mixins/rules/command_player/ServerPlayerMixin.java
         this.actionPack = new EntityPlayerActionPack((ServerPlayer) (Object) this);
     }
 
