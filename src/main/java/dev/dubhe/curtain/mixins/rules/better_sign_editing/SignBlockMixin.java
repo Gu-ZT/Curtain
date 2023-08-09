d0de0e5892de46e99afe464b078f27988a995d40
Gu_ZT <administration@ztxy666.cn>
2023-08-09 21:45:18 +0800

    feat(betterSignEditing ): 添加`更好的告示牌编辑`功能

--- src/main/java/dev/dubhe/curtain/mixins/rules/better_sign_editing/SignBlockMixin.java
+++ src/main/java/dev/dubhe/curtain/mixins/rules/better_sign_editing/SignBlockMixin.java
@@ -1,4 +1,4 @@
-package dev.dubhe.curtain.mixins.rules.better_sign_interaction;
+package dev.dubhe.curtain.mixins.rules.better_sign_editing;
 
 import dev.dubhe.curtain.CurtainRules;
 import net.minecraft.core.BlockPos;
@@ -6,9 +6,12 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.SignBlock;
 import net.minecraft.world.level.block.WallSignBlock;
+import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
@@ -21,15 +24,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class SignBlockMixin {
     private final SignBlock self = (SignBlock) (Object) this;
 
-    @Inject(method = "use", at = @At(value = "INVOKE",target = "Lnet/minecraft/world/item/ItemStack;getItem()Lnet/minecraft/world/item/Item;"), cancellable = true)
+    @Inject(method = "use", at = @At(value = "HEAD"), cancellable = true)
     public void use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit, CallbackInfoReturnable<InteractionResult> cir) {
-        if (CurtainRules.betterSignInteraction && self instanceof WallSignBlock) {
-            Direction direction = state.getValue(WallSignBlock.FACING);
-            BlockPos blockPos = pos.relative(direction, -1);
-            BlockState blockState = level.getBlockState(blockPos);
-            if (blockState.getBlock() instanceof WallSignBlock) return;
-            BlockHitResult hitResult = new BlockHitResult(Vec3.atCenterOf(blockPos), direction, blockPos, false);
-            blockState.use(level, player, hand, hitResult);
+        ItemStack itemStack = player.getItemInHand(hand);
+        String name = itemStack.getHoverName().getString();
+        if (CurtainRules.betterSignEditing && itemStack.is(Items.FEATHER) && (name.contains("pen") || name.contains("笔"))) {
+            SignBlockEntity sign = (SignBlockEntity) level.getBlockEntity(pos);
+            if (!level.isClientSide && sign != null) {
+                player.openTextEdit(sign);
+            }
             cir.setReturnValue(InteractionResult.SUCCESS);
         }
     }
