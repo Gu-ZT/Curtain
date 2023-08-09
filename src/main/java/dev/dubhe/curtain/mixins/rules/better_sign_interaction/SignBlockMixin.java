c6f2d43cc3cb79542e8510247e0de7edbbaa0c6b
Gu_ZT <administration@ztxy666.cn>
2023-08-09 15:32:12 +0800

    feat(blockPlacementIgnoreEntity): 添加`方块放置忽略实体`功能

--- src/main/java/dev/dubhe/curtain/mixins/rules/better_sign_interaction/SignBlockMixin.java
+++ src/main/java/dev/dubhe/curtain/mixins/rules/better_sign_interaction/SignBlockMixin.java
@@ -9,6 +9,8 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.SignBlock;
 import net.minecraft.world.level.block.WallSignBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
@@ -21,15 +23,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class SignBlockMixin {
     private final SignBlock self = (SignBlock) (Object) this;
 
-    @Inject(method = "use", at = @At(value = "INVOKE",target = "Lnet/minecraft/world/level/Level;playSound(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/core/BlockPos;Lnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;)V"), cancellable = true)
+    @Inject(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;playSound(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/core/BlockPos;Lnet/minecraft/sounds/SoundEvent;Lnet/minecraft/sounds/SoundSource;)V"), cancellable = true)
     public void use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit, CallbackInfoReturnable<InteractionResult> cir) {
         if (CurtainRules.betterSignInteraction && self instanceof WallSignBlock) {
+            BlockEntity entity = level.getBlockEntity(pos);
+            if (entity instanceof SignBlockEntity sign && !sign.isWaxed()) return;
             Direction direction = state.getValue(WallSignBlock.FACING);
             BlockPos blockPos = pos.relative(direction, -1);
             BlockState blockState = level.getBlockState(blockPos);
-            BlockHitResult hitResult = new BlockHitResult(Vec3.atCenterOf(blockPos), direction, blockPos, false);
             if (blockState.getBlock() instanceof WallSignBlock) return;
-            else blockState.use(level, player, hand, hitResult);
+            BlockHitResult hitResult = new BlockHitResult(Vec3.atCenterOf(blockPos), direction, blockPos, false);
+            blockState.use(level, player, hand, hitResult);
             cir.setReturnValue(InteractionResult.SUCCESS);
         }
     }
