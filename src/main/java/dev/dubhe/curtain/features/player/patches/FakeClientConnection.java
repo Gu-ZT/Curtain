54c671ad8392e97fbbcf28ef702f5e0acf13ca69
5aaee9 <7685264+5aaee9@users.noreply.github.com>
2024-02-22 21:58:34 +0800

    feat: try fix mem leak

--- src/main/java/dev/dubhe/curtain/features/player/patches/FakeClientConnection.java
+++ src/main/java/dev/dubhe/curtain/features/player/patches/FakeClientConnection.java
@@ -3,17 +3,27 @@ package dev.dubhe.curtain.features.player.patches;
 import dev.dubhe.curtain.features.player.fakes.IClientConnection;
 import io.netty.channel.embedded.EmbeddedChannel;
 import net.minecraft.network.Connection;
+import net.minecraft.network.PacketSendListener;
+import net.minecraft.network.protocol.Packet;
 import net.minecraft.network.protocol.PacketFlow;
+
+import javax.annotation.Nullable;
 
 public class FakeClientConnection extends Connection {
     public FakeClientConnection(PacketFlow packetFlow) {
         super(packetFlow);
         ((IClientConnection) this).setChannel(new EmbeddedChannel());
+
     }
 
     @Override
     public void setReadOnly() {
     }
+
+    @Override
+    public void send(Packet<?> pPacket, @Nullable PacketSendListener pSendListener) {
+    }
+
 
     @Override
     public void handleDisconnection() {
